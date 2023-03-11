0b4b32ad2958a79b46f1f9ac6a5d2aac092492c2
BRUNO-RAMIREZ <brunaisdiaz1@gmail.com>
2023-03-11 13:39:51 -0400

    Finish app gifs

--- gifs-app/src/app/app.module.ts
+++ gifs-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {SharedModule} from "./shared/shared.module";
 import {GifsModule} from "./gifs/gifs.module";
 
 import {AppComponent} from './app.component';
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import {AppComponent} from './app.component';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
   ],
